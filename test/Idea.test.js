3afbd5789ca44f78173be9cb37f79124b1f9db20
Petros Sargsian <petros.sargsian@gmail.com>
2021-04-20 18:06:31 +0300

    Added new ut

--- test/Idea.test.js
+++ test/Idea.test.js
@@ -9,6 +9,7 @@ const compiledIdea = require('../ethereum/build/Idea.json');
 
 const constructorArgs = []; 
 
+let manager;
 let accounts;
 let factory;
 let ideaAddress;
@@ -17,6 +18,7 @@ let idea;
 beforeEach(async () => {
   // get all accounts from provider
   accounts = await web3.eth.getAccounts();
+  manager = accounts[0];
 
   // get contract with abi (interface)
   let contract = new web3.eth.Contract(compiledFactory.abi);
@@ -29,14 +31,14 @@ beforeEach(async () => {
 
   // send it to the network
   factory = await contract.send({
-      from: accounts[0],
+      from: manager,
       gas: 1500000,
       gasPrice: '30000000000'
   });
 
   // create an Idea contract
   await factory.methods.createIdea('100').send({
-    from: accounts[0],
+    from: manager,
     gas: '1000000'
   });
 
@@ -50,9 +52,34 @@ beforeEach(async () => {
   );
 });
 
+/* TODO unit tests
+
+  -minimum contribution
+  -check how credits work
+  -requests
+*/
+
 describe('Ideas', () => {
-    it('deploys a factory and a idea', () => {
+    it('Deploys a factory and a idea', () => {
       assert.ok(factory.options.address);
       assert.ok(idea.options.address);
     });
+
+    it('The manager is the correct one', async () => {
+      const actualManager = await idea.methods.manager().call();
+      assert.strictEqual(actualManager, manager);
+    });
+
+    it('The contributer is added in the approvers list', async () => {
+      const contributorAddress = accounts[1];
+      await idea.methods.contribute().send({
+        from: contributorAddress,
+        gas: '1000000',
+        value: 100
+      });
+      const approver = await idea.methods.approvers(contributorAddress).call();
+      assert.ok(approver);
+    });
+
+    
 });  
