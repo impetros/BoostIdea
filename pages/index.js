b696782f37f0dfa19b3c62e1d04a7cb6d5a003f8
Petros Sargsian <petros.sargsian@gmail.com>
2021-04-24 14:26:19 +0300

    Set up react app

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BoostIdeaIndex extends Component {
+  render() {
+    return (
+        <div>
+          <h3>Ideas</h3>
+        </div>
+    );
+  }
+}
+
+export default BoostIdeaIndex;
