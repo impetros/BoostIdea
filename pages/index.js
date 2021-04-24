465f292ca409090db8d427836b7e1d5deb420d46
Petros Sargsian <petros.sargsian@gmail.com>
2021-04-24 17:24:08 +0300

    Created the first page (index)

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,51 @@
 import React, { Component } from "react";
+import { Card, Button } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
+import Layout from '../components/Layout';
+import { Link } from '../routes';
 
 class BoostIdeaIndex extends Component {
   static async getInitialProps() {
     const ideas = await factory.methods.getDeployedIdeas().call();
-    console.log(ideas);
     return { ideas };
+  }
+
+  renderIdeas() {
+    const items = this.props.ideas.map(address => {
+      return {
+        header: address,
+        description: (
+          <Link route={`/ideas/${address}`}>
+            <a>View Idea</a>
+          </Link>
+        ),
+        fluid: true
+      };
+    });
+
+    return <Card.Group items={items} />;
   }
 
   render() {
     return (
-      <div>
-        <h3>Ideas</h3>
-      </div>
+      <Layout>
+        <div>
+          <h3>Open Ideas</h3>
+
+          <Link route="/ideas/new">
+            <a>
+              <Button
+                floated="right"
+                content="Create Idea"
+                icon="add circle"
+                primary
+              />
+            </a>
+          </Link>
+
+          {this.renderIdeas()}
+        </div>
+      </Layout>
     );
   }
 }
