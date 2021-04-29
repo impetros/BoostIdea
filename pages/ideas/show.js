6cc8b3aee71b5a604af188de00815ee2f5646433
Petros Sargsian <petros.sargsian@gmail.com>
2021-04-29 17:48:20 +0300

    Now we are displaying if an idea has charitable purposes

--- pages/ideas/show.js
+++ pages/ideas/show.js
@@ -15,7 +15,6 @@ class IdeaShow extends Component {
 
     const summary = await idea.methods.getSummary().call();
     const moreDetails = await idea.methods.getMoreDetails().call();
-
     return {
       address: address,
       name: summary[0],
@@ -36,17 +35,8 @@ class IdeaShow extends Component {
 
   renderCards() {
     const {
-      address,
-      name,
-      shortDescription,
-      imageURL,
-      category,
-      isDonation,
       minimumContribution,
       creditsCount,
-      createdAt,
-      reachGoal,
-      description,
       requestsCount,
       balance,
       manager,
@@ -61,8 +51,8 @@ class IdeaShow extends Component {
         style: { overflowWrap: "break-word" },
       },
       {
-        header: minimumContribution,
-        meta: "Minimum Contribution (wei)",
+        header: web3.utils.fromWei(minimumContribution, "ether"),
+        meta: "Minimum Contribution (ETH)",
         description:
           "You must contribute at least this much wei to become an approver",
       },
@@ -79,7 +69,7 @@ class IdeaShow extends Component {
       },
       {
         header: web3.utils.fromWei(balance, "ether"),
-        meta: "Idea Balance (ether)",
+        meta: "Idea Balance (ETH)",
         description:
           "The balance is how much money this idea has left to spend.",
       },
@@ -89,33 +79,61 @@ class IdeaShow extends Component {
   }
 
   render() {
-    const percent = (this.props.minimumContribution * this.props.creditsCount) / this.props.reachGoal * 100;
+    const percent =
+      ((this.props.minimumContribution * this.props.creditsCount) /
+        this.props.reachGoal) *
+      100;
     return (
       <Layout>
-        <section style={{margin: '0 20% 0 20%'}}>
+        <section style={{ margin: "0 20% 0 20%" }}>
           <Grid>
             <Grid.Row>
               <Grid.Column width={10}>
-                <Image src={this.props.imageURL} width="500" height="500"/>
+                <Image src={this.props.imageURL} width="500" height="500" />
+                {this.props.isDonation && (
+                  <div>
+                    <div
+                      style={{
+                        float: "left",
+                        fontWeight: "bold",
+                        fontSize: "2em",
+                        color: "lightpink",
+                      }}
+                    >
+                      - Charitable Purpose
+                    </div>
+                    <img
+                      src="https://img.favpng.com/17/7/7/heart-icon-love-icon-png-favpng-fdEYx0u0j6vLbKTw6ptuPxCD2.jpg"
+                      width="50"
+                      height="50"
+                      style={{ float: "left" }}
+                    ></img>
+                  </div>
+                )}
               </Grid.Column>
 
               <Grid.Column width={6}>
-                <h2 style={{fontSize: '5em', fontWeight: 'bold'}}>{this.props.name}</h2>
-                <div style={{width: '50%', float: 'right'}}>
-                    <CategorySpan categoryName={this.props.category} />
+                <h2 style={{ fontSize: "5em", fontWeight: "bold" }}>
+                  {this.props.name}
+                </h2>
+                <div style={{ width: "50%", float: "right" }}>
+                  <CategorySpan categoryName={this.props.category} />
                 </div>
-                <h4 style={{fontStyle: 'italic'}}>{this.props.shortDescription}</h4>
+                <h4 style={{ fontStyle: "italic" }}>
+                  {this.props.shortDescription}
+                </h4>
                 <p>{this.props.description}</p>
                 <div>
-                  <ProgressBar percent={percent} goal={web3.utils.fromWei(this.props.reachGoal, 'ether')}></ProgressBar>
+                  <ProgressBar
+                    percent={percent}
+                    goal={web3.utils.fromWei(this.props.reachGoal, "ether")}
+                  ></ProgressBar>
                 </div>
                 <BeAContributorForm address={this.props.address} />
               </Grid.Column>
             </Grid.Row>
             <Grid.Row>
-              <Grid.Column>
-              {this.renderCards()}
-              </Grid.Column>
+              <Grid.Column>{this.renderCards()}</Grid.Column>
             </Grid.Row>
             <Grid.Row>
               <Grid.Column>
